f84339fa9b7c7151a36b65cfd99f1889fd81b660
YoannWicker <yoann.wicker@lacombedulionvert.fr>
2022-03-07 18:01:51 +0100

    test build

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/springapp-repo
+cd /home/ec2-user/springapp-repo/infrastructure/target
 sudo /usr/bin/java -jar -Dserver.port=8080 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
