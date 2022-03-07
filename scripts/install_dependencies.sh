da89f1002804650a139ae4458c4044054c825d61
YoannWicker <yoann.wicker@lacombedulionvert.fr>
2022-03-07 15:48:01 +0100

    test build

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,2 @@
+cd /home/ec2-user/springapp-repo
+sudo /usr/bin/mvn clean install
--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cd /home/ec2-user/spring-app-target
+cd /home/ec2-user/springapp-repo
 sudo /usr/bin/java -jar -Dserver.port=8080 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
