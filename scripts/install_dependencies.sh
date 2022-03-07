26875ff36c774e34171ef537130712b4c41aa3eb
YoannWicker <yoann.wicker@lacombedulionvert.fr>
2022-03-07 17:52:33 +0100

    test build

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,5 @@
 cd /home/ec2-user/springapp-repo
 /usr/bin/date >> log.txt
-set JAVA_HOME=/usr/java/jdk-17.0.2
 echo $JAVA_HOME >> log.txt
 /usr/bin/mvn -version >> log.txt
 sudo /usr/bin/mvn clean install -DskipTests
