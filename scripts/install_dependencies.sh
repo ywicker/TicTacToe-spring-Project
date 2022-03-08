67885f7651755e1eaee07e2fb5c21c1e5d58093a
YoannWicker <yoann.wicker@lacombedulionvert.fr>
2022-03-08 11:28:22 +0100

    test deploy

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 cd /home/ec2-user/springapp-repo
 sudo /opt/apache-maven/bin/mvn clean install -DskipTests
 cd infrastructure/target
-sudo /usr/bin/mv *.jar /home/ec2-user/spring-app
+sudo /usr/bin/mv *.jar /home/ec2-user/spring-app/ticTacToeApp.jar
--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/spring-app
 sudo /usr/bin/java -jar -Dserver.port=8080 \
-    *.jar > /dev/null 2> /dev/null < /dev/null &
+    ticTacToeApp.jar > /dev/null 2> /dev/null < /dev/null &
