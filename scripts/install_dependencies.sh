a860d49ef4b6f2fa184d4d0529e5daf575029c35
YoannWicker <yoann.wicker@lacombedulionvert.fr>
2022-03-08 11:21:04 +0100

    test deploy

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,4 @@
 cd /home/ec2-user/springapp-repo
-/usr/bin/date >> log.txt
-echo $JAVA_HOME >> log.txt
-/opt/apache-maven/bin/mvn -version >> log.txt
 sudo /opt/apache-maven/bin/mvn clean install -DskipTests
-sudo /usr/bin/mv /infrastructure/target/*.jar /home/ec2-user/spring-app
+cd infrastructure/target
+sudo /usr/bin/mv *.jar /home/ec2-user/spring-app
